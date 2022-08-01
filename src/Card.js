a5ee720bcd9c074eb08047055ad38702bcfef7b4
Javier Blanco <javierblancoit@gmail.com>
2022-08-01 10:13:00 +0100

    initial config for the project

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+
+
+function Card() {
+    return (
+        <div>
+            <img src='' alt='Robot'/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
